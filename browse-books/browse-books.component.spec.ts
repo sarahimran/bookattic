ff638ddfbdb3e8d4e620005dff3589ce8bb23105
sarahimran <76063689+sarahimran@users.noreply.github.com>
2020-12-15 22:33:40 +0500

    Add files via upload

---
+++ browse-books/browse-books.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BrowseBooksComponent } from './browse-books.component';
+
+describe('BrowseBooksComponent', () => {
+  let component: BrowseBooksComponent;
+  let fixture: ComponentFixture<BrowseBooksComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BrowseBooksComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BrowseBooksComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
